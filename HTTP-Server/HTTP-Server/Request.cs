cd3db929e8137925498683666ee3a0a579847cbd
Karolina Dubitskaya <Karolina.D100899@gmail.com>
2018-05-15 22:23:05 +0300

    Add Request class

---
+++ HTTP-Server/HTTP-Server/Request.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTTP_Server
+{
+    public class Request
+    {
+        public string Method { get; }
+        public string URL { get; }
+
+        public Request(string msg)
+        {
+            if (String.IsNullOrEmpty(msg))
+            {
+                throw new ArgumentException();
+            }
+
+            char[] separators = { ' ', '\n' };
+            string[] tokens = msg.Split(separators);
+            Method = tokens[0];
+            URL = tokens[1];
+        }
+    }
+}
